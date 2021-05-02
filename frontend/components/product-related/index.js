c1672cc23854761383cb6d8d2235ab0ddb1da0ed
paradorn chandeth <paradorn@qboxpoint.com>
2021-05-03 05:51:53 +0700

    Fix responsive product relatated

--- frontend/components/product-related/index.js
+++ frontend/components/product-related/index.js
@@ -1,10 +1,32 @@
-import ProductItem from './product-item'
+import ProductItem from './product-item';
 import {Swiper, SwiperSlide} from 'swiper/react';
 export default function () {
   return (
     <div className="mt-5">
-      <h6 className="mb-3 font-weight-bold"><b>RELATED PRODUCTS</b></h6>
-      <Swiper spaceBetween={50} slidesPerView={4}>
+      <h6 className="mb-3 font-weight-bold">
+        <b>RELATED PRODUCTS</b>
+      </h6>
+      <Swiper
+        spaceBetween={50}
+        slidesPerView={4}
+        breakpoints={{
+          0: {
+            width: 0,
+            slidesPerView: 1,
+          },
+          768: {
+            width: 768,
+            slidesPerView: 2,
+          },
+          991: {
+            width: 991,
+            slidesPerView: 3,
+          },
+          1192: {
+            width: 1192,
+            slidesPerView: 4,
+          },
+        }}>
         <SwiperSlide>
           <ProductItem />
         </SwiperSlide>
--- frontend/components/product-related/product-item.js
+++ frontend/components/product-related/product-item.js
@@ -1,8 +1,7 @@
-
 import StarRatings from 'react-star-ratings';
 import NumberFormat from 'react-number-format';
 
-export default function ProductItem () {
+export default function ProductItem() {
   return (
     <>
       <div className="product-card">
@@ -11,39 +10,35 @@ export default function ProductItem () {
         </div>
         <h6 className="mt-2">HEPA Circulation Unit</h6>
         <span>เครื่องกรองอากาศควบคุมเชื้อ</span>
-        <StarRatings 
-          rating={5} 
-          starRatedColor="rgb(202, 153, 9)" 
-          numberOfStars={5} name="rating" 
-          starDimension="20" 
-          starSpacing="2px"
-        />
+        <div>
+          <StarRatings rating={5} starRatedColor="rgb(202, 153, 9)" numberOfStars={5} name="rating" starDimension="20" starSpacing="2px" />
+        </div>
         <div className="price mt-3">
-          <NumberFormat value={245698} displayType={'text'} thousandSeparator={true} suffix={" บาท"}/>
+          <NumberFormat value={245698} displayType={'text'} thousandSeparator={true} suffix={' บาท'} />
         </div>
       </div>
 
-      <style jsx>{`
-        .product-card-img {
-          width: 100%; 
-          height: 340px;
-        }  
+      <style jsx>
+        {`
+          .product-card-img {
+            width: 100%;
+            height: 340px;
+          }
 
-        .product-card h6 {
-          color: #4184e6;
-        }
+          .product-card h6 {
+            color: #4184e6;
+          }
 
-        .product-card img {
-          width: 100%;
-          height: 100%;
-          object-fit: cover;
-        }
+          .product-card img {
+            width: 100%;
+            height: 100%;
+            object-fit: cover;
+          }
 
-        .product-card .price{
-          color: red;
-        }
-
-      `}
+          .product-card .price {
+            color: red;
+          }
+        `}
       </style>
     </>
   );
