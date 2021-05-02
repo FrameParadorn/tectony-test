fafd33e9e51ebc3fca14d99531173f990386bb26
paradorn chandeth <paradorn@qboxpoint.com>
2021-05-03 00:18:07 +0700

    Add responsive to navbar

---
+++ frontend/components/navbar/desktop.js
@@ -0,0 +1,102 @@
+import React from 'react';
+import Link from 'next/link';
+import {HiOutlineShoppingBag} from 'react-icons/hi';
+import {FaRegHeart} from 'react-icons/fa';
+import {RiUserFill} from 'react-icons/ri';
+
+function Desktop({menus}) {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light d-none d-lg-block">
+      <div className="container">
+        <div className="col-4">
+          <img src="/logo.png" alt="logo" className="logo" />
+        </div>
+        <div className="col-6">
+          <ul className="navbar-nav justify-content-end">
+            {menus.main.map((menu) => (
+              <li className="nav-item bold" key={menu.title}>
+                <Link href={ menu.href }>
+                  <a className="d-inline">{menu.title}</a>
+                </Link>
+              </li>
+            ))}
+          </ul>
+          <hr />
+          <ul className="navbar-nav justify-content-end">
+            {menus.sub.map((menu, index) => {
+              let separate = !!index ? <div className="nav-item separate">|</div> : null;
+
+              return (
+                <li className="nav-item item-sub" key={index}>
+                   {separate}
+                   <Link href={ menu.href }>
+                    <a>{menu.title}</a>
+                  </Link>
+                </li>
+              );
+
+              return list;
+            })}
+          </ul>
+        </div>
+        <div className="col-2 d-flex justify-content-center nav-icon">
+          <div className="icon">
+            <HiOutlineShoppingBag />
+          </div>
+          <div className="icon">
+            <FaRegHeart />
+          </div>
+          <div className="icon">
+            <RiUserFill />
+          </div>
+        </div>
+      </div>
+      <style jsx>
+        {`
+          .container {
+            padding: 2rem 0;
+          }
+
+          .logo {
+            width: 100px;
+          }
+
+          .nav-item {
+            position: relative;
+            margin: auto 10px;
+          }
+
+          .nav-item.bold a {
+            margin: auto 20px;
+            font-weight: bold;
+          }
+
+          .nav-item.item-sub {
+            font-size: 0.8rem;
+            font-weight: 100;
+            color: gray;
+          }
+
+          .separate {
+            position: absolute;
+            top: -2px;
+            left: -20px;
+            display: inline;
+            color: #c3c3c3;
+            font-size: 0.9rem;
+          }
+
+          .nav-icon {
+            font-size: 2.5rem;
+          }
+
+          .nav-icon .icon {
+            margin: auto 5px;
+          }
+        `}
+      </style>
+    </nav>
+  );
+}
+
+export default Desktop;
---
+++ frontend/components/navbar/mobile.js
@@ -0,0 +1,78 @@
+import React from 'react';
+import Link from 'next/link';
+
+function mobile({menus}) {
+  const menuList = [];
+
+  for (const groups in menus) {
+    menus[groups].map((menu) => menuList.push(menu));
+  }
+
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light d-block d-lg-none">
+      <div className="container-fluid">
+        <Link href="/">
+          <a className="navbar-brand">
+            <img src="/logo.png" alt="logo" className="logo" />
+          </a>
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarText"
+          aria-controls="navbarText"
+          aria-expanded="false"
+          aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarText">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            {menuList.map((menu) => (
+              <li className="nav-item" key={menu.title}>
+                <Link href={menu.href}>
+                  <a>{menu.title}</a>
+                </Link>
+              </li>
+            ))}
+            <li className="nav-item">
+              <Link href="/">
+                <a>Cart <span class="badge bg-primary">0</span></a>
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link href="/">
+                <a>Wishlist</a>
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link href="/">
+                <a>User</a>
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <style jsx>
+        {`
+          .logo {
+            width: 100px;
+          }
+
+          .nav-item li {
+            list-style-type: none;
+          }
+
+          .nav-item a {
+            display: block;
+            width: 100%;
+            height: 30px;
+            padding: 0 20px;
+          }
+        `}
+      </style>
+    </nav>
+  );
+}
+
+export default mobile;
