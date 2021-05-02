bdfeb9a14898b3eb185160ce2e36be973d78e837
paradorn chandeth <paradorn@qboxpoint.com>
2021-05-03 02:38:52 +0700

    Add component add to cart

---
+++ frontend/components/product-content/add-to-cart.js
@@ -0,0 +1,42 @@
+export default function () {
+  return (
+    <div className="mt-5">
+      <input type="number" className="input-qty text-center" defaultValue="1" />
+      <button>ADD TO CART</button>
+      <style jsx>
+        {`
+          .input-qty {
+            -webkit-appearance: none;
+            outline: none;
+            border: none;
+            border-bottom: 2px solid;
+            font-size: 1.5rem;
+            font-weight: bold;
+            width: 100px;
+          }
+
+          button {
+            background: black;
+            color: white;
+            border: none;
+            padding: 10px 20px;
+            outline: none;
+            margin-left: 30px;
+            position: relative;
+            top: -3px;
+          }
+
+          button:active {
+            background: #1f1f1f;
+          }
+
+          input::-webkit-outer-spin-button,
+          input::-webkit-inner-spin-button {
+            -webkit-appearance: none;
+            margin: 0;
+          }
+        `}
+      </style>
+    </div>
+  );
+}
--- frontend/components/product-content/index.js
+++ frontend/components/product-content/index.js
@@ -1,9 +1,12 @@
 import Content from "./content"
+import AddToCart from "./add-to-cart"
 
 export default function () {
   return (
     <div>
       <Content />
+      <AddToCart />
+      
     </div>
   )
 }
